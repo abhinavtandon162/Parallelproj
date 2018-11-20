445d43b8fb69fa2db4f24194e541bbbf95797772
abhinavtandon162 <abhinavtandon162@gmail.com>
2018-11-20 22:28:37 +0530

    Add files via upload

---
+++ ParallelProjectCollections/src/com/cg/dto/Customer.java
@@ -0,0 +1,8 @@
+package com.cg.dto;
+
+public class Customer {
+	private String name;
+	private String mobileNo;
+	private Wallet wallet;
+	private Transaction transaction; 
+}
---
+++ ParallelProjectCollections/src/com/cg/ui/WalletClient.java
@@ -0,0 +1,9 @@
+package com.cg.ui;
+
+import com.cg.dao.EWalletDaoImpl;
+import com.cg.service.EWalletServiceImpl;
+
+public class WalletClient {
+	EWalletServiceImpl walletService;
+	
+}
